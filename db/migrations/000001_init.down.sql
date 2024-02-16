5c02686cbfb9827ec0423216e891ff6df84af775
Omid <omid.rasouli@hezardastan.net>
2024-02-16 11:04:56 +0330

    Edit migration down tsql

--- db/migrations/000001_init.down.sql
+++ db/migrations/000001_init.down.sql
@@ -1,14 +1,14 @@
+-- Migration for dropping the Trip table
+DROP TABLE IF EXISTS trips;
+
+-- Migration for dropping the Delay Reports table
+DROP TABLE IF EXISTS delay_reports;
+
 -- Migration for dropping the Order table
 DROP TABLE IF EXISTS orders;
 
--- Drop the Vendor table
+-- Migration for dropping the Vendor table
 DROP TABLE IF EXISTS vendors;
 
--- Drop the Agent table
+-- Migration for dropping the Agent table
 DROP TABLE IF EXISTS agents;
-
--- Drop the Trip table
-DROP TABLE IF EXISTS trips;
-
--- Drop the Trip table
-DROP TABLE IF EXISTS trips;
